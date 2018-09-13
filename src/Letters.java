9c45de0cc08990b7c3234ef2b4c12a776f8ae035
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2018-09-13 00:39:33 -0400

    Almost through Chapter 2

---
+++ src/Letters.java
@@ -0,0 +1,14 @@
+// This Program demonstrates the char type.
+
+public class Letters
+{
+    public static void main(String[] args)
+    {
+        char letter;
+
+        letter = 'A';
+        System.out.println(letter);
+        letter = 'B';
+        System.out.println(letter);
+    }
+}
